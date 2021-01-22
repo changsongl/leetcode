af44e3a186e1ad3c4ec8cf267b10096f8542c159
Jason li <changsongliqd@gmail.com>
2021-01-22 09:28:43 +0800

    add better memory cost

--- 001-050/013. 罗马数字转整数/main.go
+++ 001-050/013. 罗马数字转整数/main.go
@@ -1,5 +1,30 @@
 package main
 
+func romanToIntBetter(s string) int {
+	numMap := map[int32]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	num, lastValue := 0, 0
+
+	for _, b := range s {
+		num += numMap[b]
+		current := numMap[b]
+		if current > lastValue {
+			num -= 2 * lastValue
+		}
+
+		lastValue = current
+	}
+
+	return num
+}
+
 // 执行结果：通过显示详情执行用时：0 ms, 在所有 Go 提交中击败了 `100.00%` 的用户
 // 内存消耗：3.1 MB, 在所有 Go 提交中击败了 `46.02%` 的用户
 // 一次通过，空间换时间。
