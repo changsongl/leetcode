38602290778ffd6662a288121c5d980a32386011
Jason li <changsongliqd@gmail.com>
2021-01-29 14:49:34 +0800

    comment test case

--- 0050-0100/053. 最大子序和/main.go
+++ 0050-0100/053. 最大子序和/main.go
@@ -1,10 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
 	//[-2,1,-3,4,-1,2,1,-5,4]
-	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+	//fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
 }
 
 //func maxSubArray(nums []int) int {
