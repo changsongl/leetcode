8bbc07b23eb3473f59602ef892606464b8f08bd0
Jason li <changsongliqd@gmail.com>
2021-02-05 15:04:24 +0800

    climb

---
+++ 0051-0100/070. 爬楼梯/main.go
@@ -0,0 +1,17 @@
+package main
+
+func climbStairs(n int) int {
+	if n < 3 {
+		return n
+	}
+
+	dp := make([]int, n)
+	dp[0] = 1
+	dp[1] = 2
+
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n-1]
+}
