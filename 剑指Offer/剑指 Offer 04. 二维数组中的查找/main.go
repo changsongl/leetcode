8c5c1d2d9e9ffc9260296246d24855e3a8dbc7e1
Jason li <changsongliqd@gmail.com>
2021-02-13 02:29:10 +0800

    find number in matrix

---
+++ 剑指Offer/剑指 Offer 04. 二维数组中的查找/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(findNumberIn2DArray(
+		[][]int{
+			{1, 4, 7, 11, 15},
+			{2, 5, 8, 12, 19},
+			{3, 6, 9, 16, 22},
+			{10, 13, 14, 17, 24},
+			{18, 21, 23, 26, 30},
+		}, 5,
+	))
+}
+
+//执行用时：24 ms, 在所有 Go 提交中击败了96.60%的用户
+//内存消耗：6.6 MB, 在所有 Go 提交中击败了71.37%的用户
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+
+	rows, cols := len(matrix), len(matrix[0])
+	r, c := 0, 0
+	for r >= 0 && r < rows && c >= 0 && c < cols {
+		num := matrix[r][c]
+		if num == target {
+			return true
+		}
+
+		if num > target {
+			c--
+		} else if c+1 < cols && matrix[r][c+1] <= target {
+			c++
+		} else {
+			r++
+		}
+
+	}
+
+	return false
+}
