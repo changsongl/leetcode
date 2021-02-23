362eff7b38e699e6e0a262265d1e19836c19bf83
Jason li <changsongliqd@gmail.com>
2021-02-23 12:49:48 +0800

    CQueue

---
+++ 剑指Offer/剑指 Offer 09. 用两个栈实现队列/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "container/list"
+
+type CQueue struct {
+	stack1, stack2 *list.List
+}
+
+func Constructor() CQueue {
+	return CQueue{
+		stack1: list.New(),
+		stack2: list.New(),
+	}
+}
+
+func (q *CQueue) AppendTail(value int) {
+	q.stack1.PushBack(value)
+}
+
+func (q *CQueue) DeleteHead() int {
+	if q.stack2.Len() == 0 {
+		for q.stack1.Len() > 0 {
+			q.stack2.PushBack(q.stack1.Remove(q.stack1.Back()))
+		}
+	}
+
+	if q.stack2.Len() != 0 {
+		e := q.stack2.Back()
+		q.stack2.Remove(e)
+		return e.Value.(int)
+	}
+
+	return -1
+}
