c8c929cf634c9d0b9362899b4a37af001fc8e4cc
Jason li <changsongliqd@gmail.com>
2021-02-24 18:43:30 +0800

    refactor calculation

--- 剑指Offer/剑指 Offer 10- II. 青蛙跳台阶问题/main.go
+++ 剑指Offer/剑指 Offer 10- II. 青蛙跳台阶问题/main.go
@@ -3,7 +3,7 @@ package main
 func numWays(n int) int {
 	var a, b int64 = 1, 1
 	for i := 0; i < n; i++ {
-		a, b = b%1000000007, (a+b)%1000000007
+		a, b = b, (a+b)%1000000007
 	}
 
 	return int(a % 1000000007)
