eb55d6e28de8b833c1977dafb6cf5b1c752442eb
Jason li <changsongliqd@gmail.com>
2021-04-13 13:16:51 +0800

    min number

--- golang/剑指Offer/剑指 Offer 45. 把数组排成最小的数/main.go
+++ golang/剑指Offer/剑指 Offer 45. 把数组排成最小的数/main.go
@@ -5,12 +5,28 @@ import (
 	"fmt"
 	"sort"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println(minNumber([]int{1, 0}))
 }
 
+// easy to read
+func minNumberString(nums []int) string {
+	strs := make([]string, 0, len(nums))
+	for _, num := range nums {
+		strs = append(strs, strconv.Itoa(num))
+	}
+
+	sort.Slice(strs, func(i, j int) bool {
+		return strs[i]+strs[j] < strs[j]+strs[i]
+	})
+
+	return strings.Join(strs, "")
+}
+
+// fast
 func minNumber(nums []int) string {
 	sort.Slice(nums, func(i, j int) bool {
 		i, j, is, js := nums[i], nums[j], 10, 10
