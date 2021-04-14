7226ac1ff1819a775fe22fe36c8aaca37bc2d86a
Jason li <changsongliqd@gmail.com>
2021-04-14 13:25:30 +0800

    max sliding window

--- golang/剑指Offer/剑指 Offer 59 - I. 滑动窗口的最大值/main.go
+++ golang/剑指Offer/剑指 Offer 59 - I. 滑动窗口的最大值/main.go
@@ -6,8 +6,45 @@ import (
 )
 
 func main() {
-	fmt.Println(maxSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 1))
+	fmt.Println(maxSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
 }
+
+// 分组，prefix 和 suffix 通过对比 两部分 拿出区间内最高值
+//func maxSlidingWindow(nums []int, k int) []int {
+//	n := len(nums)
+//	prefixMax := make([]int, n)
+//	suffixMax := make([]int, n)
+//	for i, v := range nums {
+//		if i%k == 0 {
+//			prefixMax[i] = v
+//		} else {
+//			prefixMax[i] = max(prefixMax[i-1], v)
+//		}
+//	}
+//
+//	for i := n - 1; i >= 0; i-- {
+//		if i == n-1 || (i+1)%k == 0 {
+//			suffixMax[i] = nums[i]
+//		} else {
+//			suffixMax[i] = max(suffixMax[i+1], nums[i])
+//		}
+//	}
+//
+//	fmt.Println(prefixMax, suffixMax)
+//
+//	ans := make([]int, n-k+1)
+//	for i := range ans {
+//		ans[i] = max(suffixMax[i], prefixMax[i+k-1])
+//	}
+//	return ans
+//}
+//
+//func max(a, b int) int {
+//	if a > b {
+//		return a
+//	}
+//	return b
+//}
 
 func maxSlidingWindow(nums []int, k int) []int {
 	hp := NewHeap(len(nums), nums)
@@ -15,13 +52,16 @@ func maxSlidingWindow(nums []int, k int) []int {
 	n := len(nums)
 	ans := make([]int, 0, n-k)
 	for i := 0; i < n; i++ {
-		heap.Push(&hp, i)
 
 		if i >= k-1 {
-			for len(hp.S) > 0 && hp.S[0] < i-k+1 {
+			num := nums[i]
+			for len(hp.S) > 0 && (hp.S[0] < i-k+1 || num > hp.Top()) {
 				heap.Pop(&hp)
 			}
+			heap.Push(&hp, i)
 			ans = append(ans, hp.Top())
+		} else {
+			heap.Push(&hp, i)
 		}
 	}
 
