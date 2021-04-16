8fa80f8b841630339e63862cf8a21513a884f287
Jason li <changsongliqd@gmail.com>
2021-04-16 13:15:46 +0800

    jump

--- golang/0001-0050/045. 跳跃游戏 II/main.go
+++ golang/0001-0050/045. 跳跃游戏 II/main.go
@@ -10,19 +10,40 @@ func main() {
 
 func jump(nums []int) int {
 	n := len(nums)
-	if n == 0 {
-		return 0
-	}
-	dp := make([]int, n)
+	steps, maxPos, end := 0, 0, 0
 
 	for i := 0; i < n-1; i++ {
-		nextStep := dp[i] + 1
-		for step := 1; step <= nums[i] && i+step < n; step++ {
-			if dp[i+step] == 0 || nextStep < dp[i+step] {
-				dp[i+step] = nextStep
-			}
+		maxPos = max(maxPos, nums[i]+i)
+		if i == end {
+			end = maxPos
+			steps++
 		}
 	}
-
-	return dp[n-1]
+	return steps
 }
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+//func jumpDP(nums []int) int {
+//	n := len(nums)
+//	if n == 0 {
+//		return 0
+//	}
+//	dp := make([]int, n)
+//
+//	for i := 0; i < n-1; i++ {
+//		nextStep := dp[i] + 1
+//		for step := 1; step <= nums[i] && i+step < n; step++ {
+//			if dp[i+step] == 0 || nextStep < dp[i+step] {
+//				dp[i+step] = nextStep
+//			}
+//		}
+//	}
+//
+//	return dp[n-1]
+//}
