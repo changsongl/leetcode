b937f9aaa2a219132b8cae7c417c6dca227a3e52
Jason li <changsongliqd@gmail.com>
2021-05-06 11:52:05 +0800

    push

--- golang/0401-0450/0403. 青蛙过河/main.go
+++ golang/0401-0450/0403. 青蛙过河/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println(canCross([]int{0, 1, 3, 5, 6, 8, 12, 17}))
 	fmt.Println(canCross([]int{0, 1, 2, 3, 4, 8, 9, 11}))
-	fmt.Println(canCross([]int{0, 2}))
+	//fmt.Println(canCross([]int{0, 2}))
 }
 
 func canCross(stones []int) bool {
