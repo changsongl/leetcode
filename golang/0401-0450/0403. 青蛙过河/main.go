6db913396a4110d18b8a574ba6ec5c9c68977138
Jason li <changsongliqd@gmail.com>
2021-05-06 12:00:26 +0800

    comment code

--- golang/0401-0450/0403. 青蛙过河/main.go
+++ golang/0401-0450/0403. 青蛙过河/main.go
@@ -1,10 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(canCross([]int{0, 1, 3, 5, 6, 8, 12, 17}))
-	fmt.Println(canCross([]int{0, 1, 2, 3, 4, 8, 9, 11}))
+	//fmt.Println(canCross([]int{0, 1, 3, 5, 6, 8, 12, 17}))
+	//fmt.Println(canCross([]int{0, 1, 2, 3, 4, 8, 9, 11}))
 	//fmt.Println(canCross([]int{0, 2}))
 }
 
