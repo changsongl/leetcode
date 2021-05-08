1eafb46b2f9cebdb049b1b0d000fd6f0fc14f17d
Jason li <changsongliqd@gmail.com>
2021-05-08 15:03:22 +0800

    minimum time req

---
+++ golang/1701-1750/1723. 完成所有工作的最短时间/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	//fmt.Println(minimumTimeRequired([]int{1, 2, 4, 7, 8}, 2))
+	//fmt.Println(minimumTimeRequired([]int{3, 2, 3}, 3))
+	//fmt.Println(minimumTimeRequired([]int{11, 2, 20, 18, 2, 1, 7, 11, 7, 10}, 9))
+	//[9899456,8291115,9477657,9288480,5146275,7697968,8573153,3582365,3758448,9881935,2420271,4542202]
+	fmt.Println(minimumTimeRequired([]int{250, 250, 256, 251, 254, 254, 251, 255, 250, 252, 254, 255}, 10))
+	fmt.Println(minimumTimeRequired([]int{9899456, 8291115, 9477657, 9288480, 5146275, 7697968, 8573153, 3582365, 3758448, 9881935, 2420271, 4542202}, 9))
+}
+
+// 超时
+func minimumTimeRequired(jobs []int, k int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(jobs)))
+	n := len(jobs)
+	left, right := jobs[0], 0
+	for _, job := range jobs {
+		right += job
+	}
+
+	return left + sort.Search(right-left, func(i int) bool {
+		limit := left + i
+		workers := make([]int, k)
+		var backtrace func(index int) bool
+		backtrace = func(index int) bool {
+			if index == n {
+				return true
+			}
+
+			curJob := jobs[index]
+			for i := 0; i < k; i++ {
+				if workers[i]+curJob <= limit {
+					workers[i] += curJob
+					if backtrace(index + 1) {
+						return true
+					}
+					workers[i] -= curJob
+				}
+
+				if workers[i] == 0 || workers[i]+curJob == limit {
+					return false
+				}
+			}
+
+			return false
+		}
+
+		return backtrace(0)
+	})
+}
---
+++ golang/1701-1750/1723. 完成所有工作的最短时间/overtime.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(minimumTimeRequiredOvertime([]int{1, 2, 4, 7, 8}, 2))
+	fmt.Println(minimumTimeRequiredOvertime([]int{3, 2, 3}, 3))
+	fmt.Println(minimumTimeRequiredOvertime([]int{11, 2, 20, 18, 2, 1, 7, 11, 7, 10}, 9))
+	////[9899456,8291115,9477657,9288480,5146275,7697968,8573153,3582365,3758448,9881935,2420271,4542202]
+	fmt.Println(minimumTimeRequiredOvertime([]int{9899456, 8291115, 9477657, 9288480, 5146275, 7697968, 8573153, 3582365, 3758448, 9881935, 2420271, 4542202}, 9))
+}
+
+// 超时
+func minimumTimeRequiredOvertime(jobs []int, k int) int {
+	sort.Ints(jobs)
+	totals := make([]int, k)
+	n := len(jobs)
+	ans := 0
+	for i := 0; i < n && i < n/k+1; i++ {
+		ans += jobs[n-i-1]
+	}
+
+	var dfs func(index int)
+	dfs = func(index int) {
+		if index < 0 {
+			m := max(totals)
+			if max(totals) < ans {
+				ans = m
+			}
+			return
+		}
+
+		for i := 0; i < k; i++ {
+			if totals[i]+jobs[index] < ans {
+				totals[i] += jobs[index]
+				dfs(index - 1)
+				totals[i] -= jobs[index]
+			}
+		}
+	}
+	dfs(n - 1)
+	return ans
+}
+
+func max(totals []int) int {
+	m := totals[0]
+	for i := 1; i < len(totals); i++ {
+		if totals[i] > m {
+			m = totals[i]
+		}
+	}
+	return m
+}
