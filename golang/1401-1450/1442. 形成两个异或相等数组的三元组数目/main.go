ea9b2852dcea4d3df688e70c350c2511b5c79ce5
Jason li <changsongliqd@gmail.com>
2021-05-18 10:29:25 +0800

    count triplets o^2

--- golang/1401-1450/1442. 形成两个异或相等数组的三元组数目/main.go
+++ golang/1401-1450/1442. 形成两个异或相等数组的三元组数目/main.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Println(countTriplets([]int{2, 3, 1, 6, 7}))
 }
 
+// O(n^2)
 func countTriplets(arr []int) int {
 	n := len(arr)
 	for i := 1; i < n; i++ {
@@ -15,19 +16,43 @@ func countTriplets(arr []int) int {
 
 	for i := 0; i < n-1; i++ {
 		for j := i + 1; j < n; j++ {
-			for k := j; k < n; k++ {
-				a := arr[j-1]
-				if i != 0 {
-					a ^= arr[i-1]
-				}
+			num := arr[j]
+			if i != 0 {
+				num ^= arr[i-1]
+			}
 
-				b := arr[k] ^ arr[j-1]
-
-				if a == b {
-					ans++
-				}
+			if num == 0 {
+				ans += j - i
 			}
 		}
 	}
+
 	return ans
 }
+
+// O(n^3)
+//func countTriplets(arr []int) int {
+//	n := len(arr)
+//	for i := 1; i < n; i++ {
+//		arr[i] ^= arr[i-1]
+//	}
+//	ans := 0
+//
+//	for i := 0; i < n-1; i++ {
+//		for j := i + 1; j < n; j++ {
+//			for k := j; k < n; k++ {
+//				a := arr[j-1]
+//				if i != 0 {
+//					a ^= arr[i-1]
+//				}
+//
+//				b := arr[k] ^ arr[j-1]
+//
+//				if a == b {
+//					ans++
+//				}
+//			}
+//		}
+//	}
+//	return ans
+//}
