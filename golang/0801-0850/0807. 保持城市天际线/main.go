859c1aa4aa06ec096fdda2de303f66c7ae11bbb1
changsongl <changsongliqd@gmail.com>
2021-12-13 10:38:51 +0800

    daily

---
+++ golang/0801-0850/0807. 保持城市天际线/main.go
@@ -0,0 +1,43 @@
+package main
+
+func maxIncreaseKeepingSkyline(grid [][]int) int {
+	n := len(grid)
+	m := len(grid[0])
+	top, left := make([]int, m), make([]int, n)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			left[i] = max(left[i], grid[i][j])
+			top[j] = max(top[j], grid[i][j])
+		}
+	}
+
+	total := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			line := min(left[i], top[j])
+
+			if grid[i][j] < line {
+				total += line - grid[i][j]
+			}
+		}
+	}
+
+	return total
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+
+	return a
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
